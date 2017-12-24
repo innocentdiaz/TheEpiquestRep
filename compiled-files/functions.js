ccf9634fdd21f3c533edfcaba3a211e7ac44f4e8
unknown <diaz.john312@gmail.com>
2017-12-24 14:54:23 -0500

    New button design!

--- compiled-files/functions.js
+++ compiled-files/functions.js
@@ -1,1 +1,31 @@
-var playtheme,push,question,submit,updatestats;window.displayToPlayer=function(message){return $("#display").html(message)},question="",push=function(item){return user.inventory.push(item)},updatestats=function(){return $("#name").text(user.name),$("#money").text(user.money),$("#safe").text(user.safe),$("#rod").text(user.rod),$("#level").text(user.lvl),$("#xp").text(user.xp),$("#armor").text(user.armor),$("#weapon").text(user.weapon),localStorage.player=JSON.stringify(user)},playtheme=function(selected){switch(selected){case"mario":return mario.loop=!0,mario.play()}},submit=function(event){if(13===event.keyCode)return question=$("#inputBox").val(),$("#inputBox").val(""),current()};
+var playtheme, push, question, updatestats;
+
+window.displayToPlayer = function(message) {
+  return $('#display').html(message);
+};
+
+question = '';
+
+push = function(item) {
+  return user.inventory.push(item);
+};
+
+updatestats = function() {
+  $('#name').text(user.name);
+  $('#money').text(user.money);
+  $('#safe').text(user.safe);
+  $('#rod').text(user.rod);
+  $('#level').text(user.lvl);
+  $('#xp').text(user.xp);
+  $('#armor').text(user.armor);
+  $('#weapon').text(user.weapon);
+  return localStorage.player = JSON.stringify(user);
+};
+
+playtheme = function(selected) {
+  switch (selected) {
+    case 'mario':
+      mario.loop = true;
+      return mario.play();
+  }
+};
